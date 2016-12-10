aea4660a0df83b2227d6e33d37694b7a859f9e10
Cychih <michael66230@gmail.com>
2016-12-11 02:27:45 +0800

    let rings rotate over distance

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ $(function () {
         el: '#app',
         data: {
             frame_rate: 30,
+            frame_per_rings: 10,
+            frame_num: 0,
+            ring_pattern_period: 5,
+            ring_num: 0,
+            stars_per_ring: 20,
             stars: [],
             width: $(window).width(),
             height: $(window).height(),
-            steps: 0,
             proj_info: function (star) {
                 let proj_width = (100 - star.z) / 2 + 20;
                 let proj_x = Math.cos(star.theta) * vm.ring_radius;
@@ -14,7 +18,8 @@ $(function () {
                 let proj_top = (proj_y * (vm.screen_z / (star.z / 100 * vm.star_plane)) + vm.height / 2);
                 let proj_left = (proj_x * (vm.screen_z / (star.z / 100 * vm.star_plane)) + vm.width / 2);
                 if (proj_top < -vm.width || proj_top > vm.height * 2
-                        || proj_left < -vm.width || proj_left > vm.width * 2) {
+                        || proj_left < -vm.width || proj_left > vm.width * 2
+                        || star.z < 0) {
                     star.trash = true;
                 }
                 return {
@@ -34,12 +39,6 @@ $(function () {
             star_plane: function () {
                 return (vm.width + vm.height) * 2;
             },
-            stars_per_ring: function () {
-                return 20;
-            },
-            gap_between_ring: function () {
-                return 10;
-            },
             ring_radius: function () {
                 return (vm.width + vm.height) / 2;
             },
@@ -51,10 +50,10 @@ $(function () {
         vm.height = $(window).height();
     });
 
-    function generate_stars () {
+    function next_step (ring_num) {
         for (var i = 0; i < vm.stars_per_ring; i++) {
             vm.stars.push({
-                theta: i / vm.stars_per_ring * 2 * Math.PI,
+                theta: ((i + (ring_num / vm.ring_pattern_period)) / vm.stars_per_ring) * 2 * Math.PI,
                 z: 100,
                 trash: false,
             });
@@ -62,9 +61,10 @@ $(function () {
     }
 
     function animate () {
-        vm.steps = (vm.steps + 1) % vm.gap_between_ring;
-        if (vm.steps == 0) {
-            generate_stars();
+        vm.frame_num = (vm.frame_num + 1) % vm.frame_per_rings;
+        if (vm.frame_num == 0) {
+            vm.ring_num = (vm.ring_num + 1) % vm.ring_pattern_period;
+            next_step(vm.ring_num);
         }
 
         vm.stars.forEach(function (star) {
@@ -77,8 +77,6 @@ $(function () {
 
         setTimeout(animate, vm.frame_rate);
     }
-
-    generate_stars();
 
     setTimeout(animate, vm.frame_rate);
 });
