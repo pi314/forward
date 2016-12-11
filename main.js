9fb09fa3c46aa1019431908c25d7dc0de540147e
Cychih <michael66230@gmail.com>
2016-12-11 21:25:22 +0800

    implement tunnel bending

--- main.js
+++ main.js
@@ -11,14 +11,25 @@ $(function () {
             stars: [],
             width: $(window).width(),
             height: $(window).height(),
-            bend_x: 0,
-            bend_y: 0,
+            bend_angle_x: 0,
+            bend_angle_y: 0,
+            warp_rate: 1.5,
             proj_info: function (star) {
+                let bend_radius_x = Math.sin(-vm.bend_angle_x) * vm.star_plane;
+                let bend_z_angle_x = 2 * vm.bend_angle_x * star.z / 100;
+                let proj_x = (Math.cos(star.theta) * vm.ring_radius +
+                              bend_radius_x) *
+                              Math.cos(bend_z_angle_x) - bend_radius_x;
+
+                let bend_radius_y = Math.sin(-vm.bend_angle_y) * vm.star_plane;
+                let bend_z_angle_y = 2 * vm.bend_angle_y * star.z / 100;
+                let proj_y = (Math.sin(star.theta) * vm.ring_radius +
+                              bend_radius_y) *
+                              Math.cos(bend_z_angle_y) - bend_radius_y;
+
+                let proj_top = (proj_y * (vm.screen / (star.z / 100 * vm.star_plane)) + vm.height / 2);
+                let proj_left = (proj_x * (vm.screen / (star.z / 100 * vm.star_plane)) + vm.width / 2);
                 let proj_width = (100 - star.z) / 2 + 20;
-                let proj_x = Math.cos(star.theta) * vm.ring_radius;
-                let proj_y = Math.sin(star.theta) * vm.ring_radius;
-                let proj_top = (proj_y * (vm.screen_z / (star.z / 100 * vm.star_plane)) + vm.height / 2);
-                let proj_left = (proj_x * (vm.screen_z / (star.z / 100 * vm.star_plane)) + vm.width / 2);
                 if (proj_top < -vm.width || proj_top > vm.height * 2
                         || proj_left < -vm.width || proj_left > vm.width * 2
                         || star.z < 0) {
@@ -34,27 +45,18 @@ $(function () {
                 };
             },
         },
-        methods: {
-            test: function () {
-                return {
-                    width: '5px',
-                    height: '5px',
-                    background: 'white',
-                    position: 'absolute',
-                    top: (vm.bend_y * vm.ring_radius + vm.height / 2) + 'px',
-                    left: (vm.bend_x * vm.ring_radius + vm.width / 2) + 'px',
-                };
-            },
-        },
         computed: {
-            screen_z: function () {
+            screen: function () {
                 return Math.min(vm.width, vm.height);
             },
             star_plane: function () {
                 return (vm.width + vm.height) * 2;
             },
             ring_radius: function () {
-                return (vm.width + vm.height) / 2;
+                return Math.min(vm.width, vm.height);
+            },
+            bend_limit: function () {
+                return Math.atan(vm.width / 2, screen);
             },
         }
     });
@@ -67,9 +69,8 @@ $(function () {
     $(window).mousemove(function (evt) {
         let mouse_x = evt.clientX - vm.width / 2;
         let mouse_y = evt.clientY - vm.height / 2;
-        let upper_bound = Math.max(vm.width, vm.height);
-        vm.bend_x = mouse_x / upper_bound;
-        vm.bend_y = mouse_y / upper_bound;
+        vm.bend_angle_x = (mouse_x / vm.width / vm.warp_rate) * vm.bend_limit;
+        vm.bend_angle_y = (mouse_y / vm.height / vm.warp_rate) * vm.bend_limit;
     });
 
     function next_step (ring_num) {
